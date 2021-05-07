6e33b37bafbc82c5a06d1ea049387fea36aad7a3
MDevoldere <mdevoldere@users.noreply.github.com>
2021-05-07 11:58:46 +0200

    create my-project
    A fantastic Hello world App !

---
+++ my-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
